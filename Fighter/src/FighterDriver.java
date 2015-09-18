b42d508b75e5ff866d37a71aee4b8a80a30493a1
SmurphDogg <mbwv@outlook.com>
2015-09-18 04:44:45 -0700

    Murphy commit that might actually work

--- Fighter/src/FighterDriver.java
+++ Fighter/src/FighterDriver.java
@@ -1,6 +1,6 @@
 
 public class FighterDriver {
 	public static void main(String[] args) {
-		System.out.println("Starting Fighter... Test 2");
+		System.out.println("Starting Fighter... Test 7");
 	}
 }
